3bfce835319b1d91de78bf91ec2d7dc9c507605c
James Williams <jr_williams@me.com>
2022-05-05 18:11:45 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.@(js|jsx|ts|tsx)', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@softwareimaging/tailwind')()]
+};
